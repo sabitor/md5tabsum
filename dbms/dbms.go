d158d7f49e0b65b99193d4d585f2014860085167
sabitor <sabitor@web.de>
2023-02-20 08:02:34 +0100

    Moved from root folder into dbms

---
+++ dbms/dbms.go
@@ -0,0 +1,35 @@
+package dbms
+
+import (
+	"database/sql"
+)
+
+// DBMS interface
+type dbms interface {
+	// openDB implements the DBMS specific open function.
+	openDB() (*sql.DB, error)
+	// closeDB implements the DBMS specific close function.
+	closeDB(*sql.DB) error
+	// queryDB implements any DBMS specific function using a handle returned from openDB() to work with the underlying database.
+	queryDB(*sql.DB) error
+}
+
+// Common config file structure
+type config struct {
+	instance string
+	host     string
+	port     int
+	user     string
+	schema   string
+	table    []string
+}
+
+// Dbms validates the existence of a specific DBMS instance and if found, returns the instance name.
+// If the DBMS instance wasn't found, the program will terminate.
+func Dbms(dbmsInstance string) dbms {
+	if v, ok := gDbms[dbmsInstance]; ok {
+		return v
+	}
+	msg := "key '" + dbmsInstance + "' doesn't exist"
+	panic(msg)
+}
